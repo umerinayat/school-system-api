544b1814d1a209aac5b89abf96a3d11916174bdf
Umer Inayat <umerinayat.369@gmail.com>
2020-04-30 23:30:14 +0500

    Updated  RegisterContoller and added api user registeration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use App\User;
-use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
@@ -24,22 +25,11 @@ class RegisterController extends Controller
 
     use RegistersUsers;
 
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    protected function registered(Request $request, $user)
     {
-        $this->middleware('guest');
+        return response()->json($user, 200);
     }
+
 
     /**
      * Get a validator for an incoming registration request.
@@ -50,6 +40,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
+            'username' => ['required', 'string', 'max:15', 'alpha_dash', 'unique:users,username'],
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
@@ -65,6 +56,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            'username' => $data['username'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
--- routes/api.php
+++ routes/api.php
@@ -7,17 +7,14 @@ use Illuminate\Support\Facades\Route;
 // 1: Public Routes
 
 
-
 // 2: Route Group for Guests uesrs only
 Route::group([ 'middleware' => ['guest:api'] ], function () {
-    // TODO
+    // Register New User
+    Route::post('register', 'Auth\RegisterController@register');
 });
 
 // 3: Route Group for authenticated uesrs only
 Route::group([ 'middleware' => ['auth:api'] ], function () {
 
-    // Register New User
-    Route::post('register', 'Auth\RegisterController@register');
-    
 });
 
